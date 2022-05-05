916e70d2e55e5e9d8fc2c6ef64eaf8584c6c8680
Aquitano / Thomas Breindl <Aquitano@Protonmail.com>
2022-05-05 20:02:46 +0200

    Added basic header setup + Changed title + Set default type font in TailWind config

--- website/client/tailwind.config.js
+++ website/client/tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans : ['DM Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
